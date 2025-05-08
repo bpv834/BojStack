986cbd67749c88f81d5bc02c0f2324cc0fc67eaf
bpv834 <bpv8342Gmail.com>
2025-05-08 20:04:41 +0900

    팩토리얼 0의 개수 BigInteger클래스 사용방법 // 팩의 0의 개수는 2가 사용된 개수, 5가 사용된개수 중 min 값이다

--- boj1676/src/Boj1676.java
+++ boj1676/src/Boj1676.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class Boj1676 {
-    public static void main(String[] args) throws IOException {
+   /* public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String factorial = String.valueOf(fac(n));
@@ -17,9 +17,24 @@ public class Boj1676 {
         //System.out.println(fac(n));
         System.out.println(cnt);
 
-    }
+    }*/
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int n = Integer.parseInt(br.readLine());
 
-    static BigInteger fac(int n) {
+       int tCnt=0;
+       int fCnt=0;
+       // n!에서 0의 개수는 5가 사용된 개수, 2가 사용된 개수 중 작은값과 같다
+       for(int i =2; i<=n;i*=2) tCnt+= n/i;
+       for(int i =5; i<=n;i*=5) fCnt+= n/i;
+
+       System.out.println(Math.min(tCnt,fCnt));
+
+
+       int cnt=0;
+   }
+
+/*    static BigInteger fac(int n) {
         BigInteger result = BigInteger.ONE;
         if(n==0)return result;
         if(n==1) return result;
@@ -28,5 +43,5 @@ public class Boj1676 {
 
         }
         return result;
-    }
+    }*/
 }
