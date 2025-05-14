334a2d49359718f41bc40ca7ba11eb54072cab73
bpv834 <bpv8342Gmail.com>
2025-05-14 19:27:46 +0900

    가장 긴 증가하는 부분 수열 4/리스트<리스트> dp 로 해결/prev 배열을 이용해 추적사용

--- boj14002/src/Boj14002.java
+++ boj14002/src/Boj14002.java
@@ -1,15 +1,81 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Boj14002 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] A = new int[n];
-        ArrayList<ArrayList<Integer>> dp = new ArrayList<>();
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+        Arrays.fill(prev,-1);
+        Arrays.fill(dp,1);
 
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = Math.max(dp[j] + 1, dp[i]);
+                        prev[i] = j;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        System.out.println(Arrays.toString(prev));
+        int val = -1;
+        int idx = -1;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > val) {
+                val = dp[i];
+                idx = i;
+            }
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(A[idx]);
+        while (idx > 0) {
+            int value = A[prev[idx]];
+            stack.push(value);
+            idx = prev[idx];
+        }
+
+    /*    System.out.println(stack);*/
+        StringBuilder sb = new StringBuilder();
+        System.out.println(stack.size());
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop() + " ");
+        }
+        System.out.println(sb.toString().trim());
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        ArrayList<ArrayList<Integer>> dp = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             dp.add(new ArrayList<>());
         }
@@ -38,16 +104,15 @@ public class Boj14002 {
                 idx=i;
             }
         }
-      /*  for (int i = 0; i < n; i++) {
+      *//*  for (int i = 0; i < n; i++) {
             System.out.println(dp.get(i));
-        }*/
+        }*//*
 
-        /*System.out.println(dp.get(idx));*/
+ *//*System.out.println(dp.get(idx));*//*
         System.out.println(dp.get(idx).size());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < dp.get(idx).size(); i++) {
             sb.append(A[dp.get(idx).get(i)] + " ");
         }
-        System.out.println(sb.toString().trim());
-    }
-}
+        System.out.println(sb.toString().trim());*/
+
